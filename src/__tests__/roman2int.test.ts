124b8a6581ab1bda062a1fe91f329c42dd1bbe34
Takuo Doi <takuo.doi@gmail.com>
2022-08-23 15:36:07 +0900

    add sample implementation

--- src/__tests__/roman2int.test.ts
+++ src/__tests__/roman2int.test.ts
@@ -1,5 +1,49 @@
-import { roman2int } from '../roman2int';
+import { roman2int} from '../roman2int';
 
 test('I_should_be_1', () => {
   expect(roman2int('I')).toBe(1);
+});
+
+test('II_should_be_2', () => {
+  expect(roman2int('II')).toBe(2);
+});
+
+test('III_should_be_3', () => {
+  expect(roman2int('III')).toBe(3);
+});
+
+test('IV_should_be_4', () => {
+  expect(roman2int('IV')).toBe(4);
+});
+
+test('V_should_be_5', () => {
+  expect(roman2int('V')).toBe(5);
+});
+
+test('VIII_should_be_8', () => {
+  expect(roman2int('VIII')).toBe(8);
+});
+
+test('IX_should_be_9', () => {
+  expect(roman2int('IX')).toBe(9);
+});
+
+test('X_should_be_10', () => {
+  expect(roman2int('X')).toBe(10);
+});
+
+test('XIV_should_be_14', () => {
+  expect(roman2int('XIV')).toBe(14);
+});
+
+test('XVI_should_be_16', () => {
+  expect(roman2int('XVI')).toBe(16);
+});
+
+test('XIX_should_be_19', () => {
+  expect(roman2int('XIX')).toBe(19);
+});
+
+test('XX_should_be_20', () => {
+  expect(roman2int('XX')).toBe(20);
 });
--- src/roman2int.ts
+++ src/roman2int.ts
@@ -1,4 +1,20 @@
-export const roman2int
-  = (roman: string): number => {
-    return 1
+const valueMap = new Map<string, number>([
+  ['I', 1], ['V', 5], ['X', 10],
+]);
+
+
+export const roman2int = (value: string): number => {
+  let result = 0
+  for (let i = value.length - 1; i >= 0; --i) {
+    let n = valueMap.get(value.charAt(i))
+    if (!n) {
+      continue;
+    }
+    if (result > 4 * n) {
+      result -= n;
+    } else {
+      result += n;
+    }
   }
+  return result
+}
