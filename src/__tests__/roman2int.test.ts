25708900171659c66075ffb0a52f2f400f6d66d9
Takuo Doi <takuo.doi@gmail.com>
2022-08-23 10:48:06 +0900

    Initial Commit

---
+++ src/__tests__/roman2int.test.ts
@@ -0,0 +1,5 @@
+import { roman2int } from '../roman2int';
+
+test('I_should_be_1', () => {
+  expect(roman2int('I')).toBe(1);
+});
---
+++ src/roman2int.ts
@@ -0,0 +1,4 @@
+export const roman2int
+  = (roman: string): number => {
+    return 1
+  }
